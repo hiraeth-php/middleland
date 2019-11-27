93a415bc90a28c4e1f063352e4913024f2782f2d
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-11-26 22:17:09 -0800

    Config cleanup

--- src/delegates/DispatcherDelegate.php
+++ src/delegates/DispatcherDelegate.php
@@ -13,6 +13,18 @@ use Psr\Http\Message\ResponseInterface as Response;
 class DispatcherDelegate implements Hiraeth\Delegate
 {
 	/**
+	 * Default configuration for a middleware
+	 *
+	 * @var array
+	 */
+	static $defaultConfig = [
+		'class'    => NULL,
+		'disabled' => FALSE,
+		'priority' => 50
+	];
+
+
+	/**
 	 * {@inheritDoc}
 	 */
 	static public function getClass(): string
@@ -26,18 +38,15 @@ class DispatcherDelegate implements Hiraeth\Delegate
 	 */
 	public function __invoke(Hiraeth\Application $app): object
 	{
-		$configs    = $app->getConfig('*', 'middleware', []);
 		$middleware = array();
+		$configs    = $app->getConfig('*', 'middleware', static::$defaultConfig);
 
 		usort($configs, function($a, $b) {
-			$a_priority = $a['priority'] ?? 50;
-			$b_priority = $b['priority'] ?? 50;
-
-			return $a_priority - $b_priority;
+			return $a['priority'] - $b['priority'];
 		});
 
 		foreach ($configs as $config) {
-			if (!$config || $config['disabled'] ?? FALSE) {
+			if (!$config['class'] || $config['disabled']) {
 				continue;
 			}
 
